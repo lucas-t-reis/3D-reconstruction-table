5028e00c459a51587e6e162bf82b4a6c2728ae0e
Lucas <lucas.tx.reis@gmail.com>
2023-06-06 20:08:52 -0300

    feat: add progressive acceleration to arduino camera

--- src/Arduino/step-motor-rotation.ino
+++ src/Arduino/step-motor-rotation.ino
@@ -1,27 +1,43 @@
 #include <Stepper.h>
-const double stepsPerRevolution = 2048; // steps per rev = (360º / 5.625 / (1/64))/2
-Stepper stepper(stepsPerRevolution, 8,10,9,11);
+
+const int stepsPerRevolution = 2048;
+Stepper tableStepper(stepsPerRevolution, 8, 10, 9, 11);
+Stepper cameraStepper(stepsPerRevolution, 3, 5, 4, 6);
+
+// Added variables to control stepper speed and ramp up
+int cameraSpeed = 0;
+const int MAX_SPEED = 14;
+const int rampPortions = 20; // Number of portions per full rotation around the table
+const int rotationsRatio = 6; // Ratio of structure diameter to cog diameter
 
 void setup() {
-  stepper.setSpeed(16);
   Serial.begin(9600);
   pinMode(13, OUTPUT);
 }
 
 void loop() {
-  
-  if( Serial.available() ) {
+  if (Serial.available()) {
     char c = (char) Serial.read();
-    if(c == 'c') {
-      stepper.step(stepsPerRevolution);
-      delay(500);
-      Serial.print("Clockwise\n");
-    }
-    else if(c == 'C') {
-      stepper.step(-stepsPerRevolution);
-      delay(500);
-      Serial.print("Counter-Clockwise\n");
+
+    if (c == 'c' || c == 'C') {
+      // Reset camera speed
+      cameraSpeed = 0;
+
+      int direction = (c == 'c') ? 1 : -1;
+      int stepsPerPortion = rotationsRatio * stepsPerRevolution / rampPortions;
+
+      // Continue rotating and increasing speed until max speed is reached
+      for (int i = 0; i < rampPortions && cameraSpeed < MAX_SPEED; i++) {
+        cameraSpeed++;
+        cameraStepper.setSpeed(cameraSpeed);
+        cameraStepper.step(direction * stepsPerPortion);
+
+      }
+
+      // tableStepper.setSpeed(15);
+      // tableStepper.step(stepsPerRevolution / rotationsRatio);
+
+      Serial.print((direction == 1) ? "Clockwise\n" : "Counter-Clockwise\n");
     }
   }
-
 }
