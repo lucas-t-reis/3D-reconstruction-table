3daff26705b7bb20ff46272c32ecc2bfa03c8abe
Lucas <lucas.tx.reis@gmail.com>
2023-06-06 19:18:11 -0300

    refactor: continuous input processing until EOF (^D)

--- src/camera_arduino_proxy.py
+++ src/camera_arduino_proxy.py
@@ -1,15 +1,17 @@
 import serial
 from time import sleep
 
+DEFAULT_MESSAGE = 'c - Clockwise | C - Counter clockwise: '
+
 # TODO Use ls -l /dev/ttyACM* + grep to fetch port name
 # TODO trycatch to handle resource busy
 # TODO Controlling rotation based on steps
 
-# Open a serial connection to Arduino UNO
 connection = serial.Serial('/dev/ttyACM0', 9600)
 # Wait for Arduino to reboot before issuing the first command
 sleep(2)
-data = input('c - Clockwise | C - Counter clockwise: ').encode('utf-8')
-connection.write(data)
-response = connection.readline()
-print(response)
+
+while(data := input(DEFAULT_MESSAGE).encode('utf-8')):
+    connection.write(data)
+    response = connection.readline()
+    print(response)
