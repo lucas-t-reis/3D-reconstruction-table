12321b6ef786dcfcbd6b47f7b1fa2c4cd99732f1
Lucas <lucas.tx.reis@gmail.com>
2023-06-06 19:55:28 -0300

    feat: adding argparse & modularizing

--- src/extract_frames.py
+++ src/extract_frames.py
@@ -1,28 +1,44 @@
+import os
 import cv2
+import argparse
 
-# Open the video file
-cap = cv2.VideoCapture('video.mp4')
+# Extract frames every ith of a second
+def extract_frames(video, interval, output_path):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
 
-# Define the frame rate and interval
-frame_rate = cap.get(cv2.CAP_PROP_FPS)
-interval = int(frame_rate / 5)  # Get a frame every 5th of a second
+    capture = cv2.VideoCapture(video)
 
-# Initialize variables
-frame_count = 0
-success = True
+    # Define the frame rate and interval
+    frame_rate = capture.get(cv2.CAP_PROP_FPS)
+    interval = int(frame_rate / interval)  # Get a frame every ith interval of a second
 
-# Loop through the video frames
-while success:
-    success, frame = cap.read()
-    if not success:
-        break
+    frame_count = 0
+    success = True
+    while success:
+        success, frame = capture.read()
+        if not success:
+            break
 
-    frame_count += 1
+        frame_count += 1
 
-    if frame_count % interval == 0:
-        filename = f'frame_{frame_count}.jpg'
-        cv2.imwrite(filename, frame)
+        if frame_count % interval == 0:
+            filename = f'frame_{frame_count}.jpg'
+            fullpath = os.path.join(output_path, filename)
+            cv2.imwrite(fullpath, frame)
 
+    capture.release()
+    cv2.destroyAllWindows()
 
-cap.release()
-cv2.destroyAllWindows()
+def main():
+
+    parser = argparse.ArgumentParser(description='Extract images, given interval of time, from a video.')
+    parser.add_argument('video', help='Path to video file. Ex. ~/Downloads/video.mp4.')
+    parser.add_argument('interval', type=int, help='Defines the amount of frames saved per second.')
+    parser.add_argument('output_path', help='Defines the folder to dump the images to.')
+
+    args = parser.parse_args()
+    extract_frames(args.video, args.interval, args.output_path)
+    
+if __name__ == '__main__':
+    main()
